c346508b559e1aea9c19da1dc5be1ab0e0694ae7
BalthPa <balthazarpapin@gmail.com>
2020-03-18 22:54:54 +0100

    Update files: Add Informations, Robots and sitemap

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let result = document.querySelector('#result');
 let button  = document.querySelector("#button");
+let infos  = document.querySelector("#infos");
 
 button.addEventListener("click", function(){
     function getRandomInt(max) {
@@ -7,30 +8,24 @@ button.addEventListener("click", function(){
     }
     test = getRandomInt(3);
     if(test == 0){
-        result.innerHTML = "<div class='jumbotron'><h3>You are infected</h3><img src='https://image.shutterstock.com/image-photo/coronavirus-covid-19-infected-patient-600w-1648211998.jpg' class='img-fluid'/></div>";
+        result.innerHTML = "<div><h3>You are infected</h3><img src='https://image.shutterstock.com/image-photo/coronavirus-covid-19-infected-patient-600w-1648211998.jpg' class='img-fluid'/><br><button class='btn btn-primary' id='buttonInfo' onclick='Infos()'>Infos</button></div>";
     }
     else if(test == 1){
-        result.innerHTML = "<div class='jumbotron'><h3>You are safe</h3><img src='https://image.shutterstock.com/image-photo/businessman-showing-thumbs-closeup-shot-600w-415992478.jpg' class='img-fluid'/></div>";
+        result.innerHTML = "<div><h3>You are safe</h3><img src='https://image.shutterstock.com/image-photo/businessman-showing-thumbs-closeup-shot-600w-415992478.jpg' class='img-fluid'/><br><button class='btn btn-primary' id='buttonInfo' onclick='Infos()'>Infos</button></div>";
     }
     else{
-        result.innerHTML = "<div class='jumbotron'><h3>It's too late</h3><img src='https://image.shutterstock.com/image-photo/alarm-clock-disintegrates-600w-717888439.jpg' class='img-fluid'/></div>";
+        result.innerHTML = "<div><h3>It's too late</h3><img src='https://image.shutterstock.com/image-photo/alarm-clock-disintegrates-600w-717888439.jpg' class='img-fluid'/><br><button class='btn btn-primary' id='buttonInfo' onclick='Infos()'>Infos</button></div>";
     }
     console.log(test)
 
     button.disabled = true
-})
+});
 
-/*function showRresult(){
-    var doc = document.getElementById('result');
-    test = getRandomInt(3);
-    if(test = 0){
-        doc.innerHTML = "Test 1";
-    }
-    else if(test = 1){
-        doc.innerHTML = "Test 2";
+function Infos(){
+    if (infos.style.display === 'none'){
+        infos.style.display = "block";
     }
     else{
-        doc.innerHTML = "Dur";
+        infos.style.display = "none";
     }
-
-}*/
+}
